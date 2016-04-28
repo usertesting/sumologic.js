d0495093f3a9a615725f8691227e51fa6d417660
Imad Mouaddine <imouaddine@usertesting.com>
2016-04-27 21:33:00 -0700

    Cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,5 @@
   // Karma configuration
 // Generated on Tue Apr 26 2016 21:58:03 GMT-0700 (PDT)
-var webpackConfig = require('./webpack.config.js');
-webpackConfig.entry = {};
-
 module.exports = function(config) {
   config.set({
 
@@ -27,7 +24,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-       "tests.webpack.js": ['webpack',  'sourcemap']
+       "tests.webpack.js": [ 'webpack',  'sourcemap', 'coverage']
     },
     webpack: {
       module: {
@@ -49,9 +46,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
+    reporters: ['dots'],
+    
     // web server port
     port: 9876,
 
@@ -71,7 +67,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
